e9d08675f467d07c00917e7dd32daa82eb9ae63c
David Ankenbrand <Ank19415@spengergasse.at>
2023-01-24 15:28:43 +0100

    init

---
+++ gridtime/src/main/java/at/spengergasse/gridtime/gridtime/GridtimeApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.gridtime.gridtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridtimeApplication.class, args);
+	}
+
+}
---
+++ gridtime/src/test/java/at/spengergasse/gridtime/gridtime/GridtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.gridtime.gridtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
